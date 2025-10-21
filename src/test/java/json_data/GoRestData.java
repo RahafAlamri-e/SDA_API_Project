5dc201f09fcfd421116cc0982982c6b0d7d1218e
RahafAlamri <rhfa9024@gmail.com>
2025-10-21 12:43:23 +0300

    Add BookerData and JSON files for booking tests

---
+++ src/test/java/json_data/GoRestData.java
@@ -0,0 +1,15 @@
+package json_data;
+
+public class GoRestData {
+
+    public final static String GOREST_LAST_USER = """
+            {
+              "id": 8194365,
+              "name": "Dipesh Khatri",
+              "email": "khatri_dipesh@ankunding-fadel.example",
+              "gender": "female",
+              "status": "active"
+            }
+            """;
+
+}
