ae0529f3bd7cb949343c5e83c1306d67dfdf0465
RahafAlamri <rhfa9024@gmail.com>
2025-10-23 09:40:42 +0300

    Add form data test for user registration and create base URL setup

---
+++ src/test/java/tests/C34_FormData.java
@@ -0,0 +1,65 @@
+package tests;
+
+import base_urls.ReqresBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class C34_FormData extends ReqresBaseUrl {
+    /*
+    Given
+        https://reqres.in/api/register
+    And
+          "email":"eve.holt@reqres.in"
+          "password": "pistol"
+    When
+        Send the post request
+    Then
+        Status code should be 200
+    And
+        Response body should be like this:
+        {
+            "id": 4,
+            "token": "QpwL5tke4Pnpja7X4"
+        }
+     */
+
+    @Test
+    void formDataTest() {
+
+        //Set the payload
+        Map<String, String> prams = new HashMap<>();
+        prams.put("email","eve.holt@reqres.in");
+        prams.put("password","pistol");
+
+
+        //Send the request
+        Response response = given(spec)
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .params(prams)
+                //OR:
+//                .formParam("email", "eve.holt@reqres.in")
+//                .formParam("password", "pistol")
+                .post("/register");
+
+        response.prettyPrint();
+
+        //Do assertion
+        response
+                .then()
+                .statusCode(200)
+                .body(
+                        "id", Matchers.notNullValue(),
+                        "token", Matchers.notNullValue()
+                );
+
+
+    }
+
+}
